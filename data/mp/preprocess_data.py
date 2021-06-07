fa2a2a059f25faf52e6e05076989c25e68ca5ec3
Huanyu <huanyu.li@liu.se>
2021-06-07 23:41:55 +0200

    Upload data

---
+++ data/mp/preprocess_data.py
@@ -0,0 +1,49 @@
+import json
+from collections import defaultdict
+
+def read_json_file(file='mp-1K.json'):
+    with open(file) as f:
+        data = json.load(f)
+        return data
+
+def write_json(file_formula_pretty, data):
+	with open(file_formula_pretty, 'w') as fp:
+		json.dump(data, fp)
+
+
+calculation_lst = []
+calculations_dict = defaultdict(list)
+structure_lst = []
+structures_dict = defaultdict(list)
+composition_lst = []
+compositions_dict = defaultdict(list)
+spacegroup_lst = []
+spacegroups_dict = defaultdict(list)
+band_gap_lst = []
+band_gaps_dict = defaultdict(list)
+formation_energy_lst = []
+formation_energies_dict = defaultdict(list)
+
+returned_data = read_json_file()
+
+for record in returned_data['data']:
+    material_id = record['material_id']
+    #composition_generic = record['composition_generic']
+    formula_pretty = record['formula_pretty']
+    spacegroup = record['spacegroup']['symbol']
+    band_gap = record['band_gap']
+    energy = record['energy']
+    calculations_dict['data'].append({'material_id': material_id})
+    structures_dict['data'].append({'material_id': material_id})
+    compositions_dict['data'].append({'material_id': material_id, 'formula_pretty':formula_pretty})
+    spacegroups_dict['data'].append({'material_id': material_id, 'spacegroup': spacegroup})
+    band_gaps_dict['data'].append({'material_id': material_id, 'band_gap': band_gap})
+    formation_energies_dict['data'].append({'material_id': material_id, 'energy': energy})
+    # write files
+    write_json('mp-calculation-1K.json', calculations_dict)
+    write_json('mp-structure-1K.json', structures_dict)
+    write_json('mp-composition-1K.json', compositions_dict)
+    write_json('mp-spacegroup-1K.json', spacegroups_dict)
+    write_json('mp-bandgap-1K.json', band_gaps_dict)
+    write_json('mp-formationenergy-1K.json', formation_energies_dict)
+    
---
+++ data/oqmd/preprocess_data.py
@@ -0,0 +1,50 @@
+import json
+from collections import defaultdict
+
+def read_json_file(file='oqmd-1K.json'):
+    with open(file) as f:
+        data = json.load(f)
+        return data
+
+def write_json(file_name, data):
+	with open(file_name, 'w') as fp:
+		json.dump(data, fp)
+
+
+calculation_lst = []
+calculations_dict = defaultdict(list)
+structure_lst = []
+structures_dict = defaultdict(list)
+composition_lst = []
+compositions_dict = defaultdict(list)
+spacegroup_lst = []
+spacegroups_dict = defaultdict(list)
+band_gap_lst = []
+band_gaps_dict = defaultdict(list)
+formation_energy_lst = []
+formation_energies_dict = defaultdict(list)
+
+returned_data = read_json_file()
+
+for record in returned_data['data']:
+    entry_id = record['entry_id']
+    calculation_id = record['calculation_id']
+    composition_generic = record['composition_generic']
+    name = record['name']
+    spacegroup = record['spacegroup']
+    band_gap = record['band_gap']
+    delta_e = record['delta_e']
+    calculations_dict['data'].append({'entry_id': entry_id, 'calculation_id': calculation_id})
+    structures_dict['data'].append({'entry_id': entry_id, 'calculation_id': calculation_id})
+    compositions_dict['data'].append({'entry_id': entry_id, 'calculation_id': calculation_id, 'name':name})
+    spacegroups_dict['data'].append({'entry_id': entry_id, 'calculation_id': calculation_id, 'spacegroup': spacegroup})
+    band_gaps_dict['data'].append({'entry_id': entry_id, 'calculation_id': calculation_id, 'band_gap': band_gap})
+    formation_energies_dict['data'].append({'entry_id': entry_id, 'calculation_id': calculation_id, 'delta_e': delta_e})
+    # write files
+    write_json('oqmd-calculation-1K.json', calculations_dict)
+    write_json('oqmd-structure-1K.json', structures_dict)
+    write_json('oqmd-composition-1K.json', compositions_dict)
+    write_json('oqmd-spacegroup-1K.json', spacegroups_dict)
+    write_json('oqmd-bandgap-1K.json', band_gaps_dict)
+    write_json('oqmd-formationenergy-1K.json', formation_energies_dict)
+    
